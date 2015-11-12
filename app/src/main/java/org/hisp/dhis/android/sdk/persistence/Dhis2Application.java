6f41eeeb4144fe0f42e27f99012c5d304e715e3d
Øyvind Johansen Amunundrud <oyvindam@ifi.uio.no>
2015-11-12 14:27:34 +0100

    Set application to be multidex, because of google play API

--- app/src/main/java/org/hisp/dhis/android/sdk/persistence/Dhis2Application.java
+++ app/src/main/java/org/hisp/dhis/android/sdk/persistence/Dhis2Application.java
@@ -30,6 +30,7 @@ package org.hisp.dhis.android.sdk.persistence;
 
 import android.app.Activity;
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 import com.raizlabs.android.dbflow.config.FlowManager;
 import com.squareup.otto.Bus;
@@ -41,7 +42,7 @@ import org.hisp.dhis.android.sdk.utils.MainThreadBus;
 /**
  *  Application for initiating the DbFlow Back end
  */
-public abstract class Dhis2Application extends Application {
+public abstract class Dhis2Application extends MultiDexApplication {
 
     public static Bus bus;
     public static DhisController dhisController;
